9734084261ca93a56f790eb33ed589143bda0130
Frank Denis <github@pureftpd.org>
2021-10-29 02:25:32 +0200

    Zig's generic vector support is so great

--- src/main.zig
+++ src/main.zig
@@ -154,10 +154,7 @@ pub const Morus = struct {
             self.update(t);
         }
         s = &self.s;
-        s[0][0] ^= s[1][1] ^ (s[2][0] & s[3][0]);
-        s[0][1] ^= s[1][2] ^ (s[2][1] & s[3][1]);
-        s[0][2] ^= s[1][3] ^ (s[2][2] & s[3][2]);
-        s[0][3] ^= s[1][0] ^ (s[2][3] & s[3][3]);
+        s[0] ^= Lane{ s[1][1], s[1][2], s[1][3], s[1][0] } ^ (s[2] & s[3]);
         var tag: [16]u8 = undefined;
         mem.writeIntLittle(u64, tag[0..8], s[0][0]);
         mem.writeIntLittle(u64, tag[8..16], s[0][1]);
